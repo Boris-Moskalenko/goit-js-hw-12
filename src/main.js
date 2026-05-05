70c38e213447b59e5aa409a70010230be3f6084d
Boris Moskalenko <mbboris12@gmail.com>
2026-05-05 20:30:47 +0300

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,48 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { getImagesByQuery } from "./js/pixabay-api.js";
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from "./js/render-functions.js";
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const query = form.elements["search-text"].value.trim();
+
+  if (!query) {
+    iziToast.error({ message: "Enter search query!" });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(images => {
+      if (images.length === 0) {
+        iziToast.error({
+          message:
+            "Sorry, there are no images matching your search query. Please try again!",
+        });
+        return;
+      }
+
+      createGallery(images);
+    })
+    .catch(() => {
+      iziToast.error({
+        message: "Something went wrong. Try again!",
+      });
+    })
+    .finally(() => {
+      hideLoader();
+      form.reset();
+    });
+});
